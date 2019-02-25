1c3d10e408ab56f7e210121418f618c8f04326b0
AtharvaSune <atharvasune@gmail.com>
2019-02-26 02:49:06 +0530

    Started front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,14 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import NGO from './components/NGO.js';
-import './NGOS.css';
+import Navbar from './components/Navbar.js';
+// import './NGOS.css';
 
 class App extends Component {
-  state={
-    ngos:[
-      {
-        id: 1,
-        title: 'Deesha',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Being Human',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Red Cross',
-        completed: false
-      }
-    ]
-  }
   render() {
     return (
       <div className="App">
-        <NGO Ngos={this.state.ngos}/>
+        <Navbar />
       </div>
     );
   }
