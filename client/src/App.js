41b4ada112c541cbc06a39c29beab99603053c26
Ameya <salankaras1234@gmail.com>
2019-03-22 19:26:16 +0530

    client: Landing page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,25 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import NGO from './components/NGO.js';
-import './NGOS.css';
+import withRoot from "./modules/withRoot";
+// --- Post bootstrap -----
+import React from "react";
+// import ProductCategories from './modules/views/ProductCategories';
+import ProductSmokingHero from "./modules/views/ProductSmokingHero";
+import AppFooter from "./modules/views/AppFooter";
+import ProductHero from "./modules/views/ProductHero";
+// import ProductValues from './modules/views/ProductValues';
+import ProductHowItWorks from "./modules/views/ProductHowItWorks";
+// import ProductCTA from './modules/views/ProductCTA';
+import AppAppBar from "./modules/views/AppAppBar";
 
-class App extends Component {
-  state={
-    ngos:[
-      {
-        id: 1,
-        title: 'Deesha',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Being Human',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Red Cross',
-        completed: false
-      }
-    ]
-  }
-  render() {
-    return (
-      <div className="App">
-        <NGO Ngos={this.state.ngos}/>
-      </div>
-    );
-  }
+function Index() {
+  return (
+    <React.Fragment>
+      <AppAppBar />
+      <ProductHero />
+      <ProductHowItWorks />
+      <ProductSmokingHero />
+      <AppFooter />
+    </React.Fragment>
+  );
 }
 
-export default App;
+export default withRoot(Index);
