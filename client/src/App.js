4831dace7a9b2203e62c0a93e8073d208380825d
AtharvaSune <atharvasune@gmail.com>
2019-03-26 22:26:05 +0530

    Landing Component Separated

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import withRoot from "./modules/withRoot";
-// --- Post bootstrap -----
-import React from "react";
-// import ProductCategories from './modules/views/ProductCategories';
-import ProductSmokingHero from "./modules/views/ProductSmokingHero";
-import AppFooter from "./modules/views/AppFooter";
-import ProductHero from "./modules/views/ProductHero";
-// import ProductValues from './modules/views/ProductValues';
-import ProductHowItWorks from "./modules/views/ProductHowItWorks";
-// import ProductCTA from './modules/views/ProductCTA';
-import AppAppBar from "./modules/views/AppAppBar";
+// import withRoot from "./modules/withRoot";
+// // --- Post bootstrap -----
+// import React from "react";
+// // import ProductCategories from './modules/views/ProductCategories';
+// import ProductSmokingHero from "./modules/views/ProductSmokingHero";
+// import AppFooter from "./modules/views/AppFooter";
+// import ProductHero from "./modules/views/ProductHero";
+// // import ProductValues from './modules/views/ProductValues';
+// import ProductHowItWorks from "./modules/views/ProductHowItWorks";
+// // import ProductCTA from './modules/views/ProductCTA';
+// import AppAppBar from "./modules/views/AppAppBar";
 
-function Index() {
-  return (
-    <React.Fragment>
-      <AppAppBar />
-      <ProductHero />
-      <ProductHowItWorks />
-      <ProductSmokingHero />
-      <AppFooter />
-    </React.Fragment>
-  );
+// function Index() {
+//   return (
+//     <React.Fragment>
+//       <AppAppBar />
+//       <ProductHero />
+//       <ProductHowItWorks />
+//       <ProductSmokingHero />
+//       <AppFooter />
+//     </React.Fragment>
+//   );
+// }
+
+// export default withRoot(Index);
+import React, { Component } from "react";
+import Landing from "./pages/Landing";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <h1>Hello</h1> */}
+        <Landing />;
+      </div>
+    );
+  }
 }
 
-export default withRoot(Index);
+export default App;
